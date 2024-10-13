80d13018128c0f43859c112e1559941ab4a183cd
roboflover <96958044+roboflover@users.noreply.github.com>
2024-10-13 03:46:30 +0300

    ss

---
+++ _nuxt/LJAUCP-f.js
@@ -0,0 +1,1 @@
+import{b as s,V as t}from"./DWEoi87O.js";import{ar as e,_ as o,ac as r,A as a,t as c,z as n}from"./LtJP8o6U.js";import{V as p}from"./C4x_1256.js";const _=e("/logo.png"),i={setup(){return{store:s()}},mounted(){}};function m(u,f,d,l,x,V){return c(),r(t,{class:"mb-5"},{default:a(()=>[n(p,{src:_})]),_:1})}const h=o(i,[["render",m]]);export{h as default};
